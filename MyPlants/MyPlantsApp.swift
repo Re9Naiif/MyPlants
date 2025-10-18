9b1d1dbb60796003c3ab9e9d5b410d67c67f185e
Reham Naif <ralotaibi25@twq.idserve.net>
2025-10-18 15:00:16 +0300

    Initial Commit

---
+++ MyPlants/MyPlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlantsApp.swift
+//  MyPlants
+//
+//  Created by reham naif  on 26/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct MyPlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
